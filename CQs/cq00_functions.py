a9a267d352dc5e058e9e49c03a15a5825a9a12cf
ssyd448 <ssyd@ad.unc.edu>
2024-08-28 09:45:57 -0400

    Challenge Question 8/28

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,16 @@
+__author__ = 730472776
+"""First Challenge Question"""
+
+
+def mimic(message: str) -> str:
+    """A Greeting Message function definition"""
+    return message
+
+
+def main() -> None:
+    """Define signature using Main function definition"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
